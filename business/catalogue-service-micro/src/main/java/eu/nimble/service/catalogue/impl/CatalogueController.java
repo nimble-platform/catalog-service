88cd8abe110f59202920e392b70d1fec32a91f7b
Suat Gonul <suat@srdc.com.tr>
2017-04-27 15:33:23 +0300

    Added a temporary companyId parameter to template publish service until the proper authentication is in place

--- business/catalogue-service-micro/src/main/java/eu/nimble/service/catalogue/impl/CatalogueController.java
+++ business/catalogue-service-micro/src/main/java/eu/nimble/service/catalogue/impl/CatalogueController.java
@@ -4,14 +4,18 @@ import eu.nimble.service.catalogue.CatalogueService;
 import eu.nimble.service.model.modaml.catalogue.TEXCatalogType;
 import eu.nimble.service.model.ubl.catalogue.CatalogueType;
 import eu.nimble.utility.Configuration;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Controller
 public class CatalogueController {
@@ -19,7 +23,7 @@ public class CatalogueController {
 	private static Logger log = LoggerFactory
 		.getLogger(CatalogueController.class);
 
-	private CatalogueService service = CatalogueServiceImpl.getInstance();
+    private CatalogueService service = CatalogueServiceImpl.getInstance();
 
 	@RequestMapping(value = "/catalogue/ubl",
 		method = RequestMethod.POST)
@@ -51,4 +55,37 @@ public class CatalogueController {
 		TEXCatalogType catalogue = (TEXCatalogType) service.getCatalogueByUUID(uuid, Configuration.Standard.MODAML);
 		return ResponseEntity.ok(catalogue);
 	}
+
+    @RequestMapping(value = "/catalogue/template",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void downloadTemplate(@RequestParam String categoryId, HttpServletResponse response) throws IOException {
+        Workbook template = service.generateTemplateForCategory(categoryId);
+
+        FileOutputStream fileOut = null;
+        try {
+            fileOut = new FileOutputStream(categoryId + "-catalogue-template.xlsx");
+            template.write(fileOut);
+            fileOut.close();
+        } catch (java.io.IOException e) {
+            log.error("Failed to create template for category: " + categoryId, e);
+        }
+
+        template.write(response.getOutputStream());
+        response.flushBuffer();
+    }
+
+    @RequestMapping(value = "/catalogue/template/upload", method = RequestMethod.POST)
+    public ResponseEntity uploadTemplate(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("companyId") String companyId) {
+        try {
+            //TODO construct the party instance properly
+            service.addCatalogue(null, file.getInputStream());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().build();
+    }
 }
