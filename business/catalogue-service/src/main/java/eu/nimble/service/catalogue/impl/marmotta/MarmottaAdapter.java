ab388dae39735b0dc19c10553ebcf0e660573160
dglachs <dglachs@gmail.com>
2017-07-11 16:37:02 +0200

    adopted to use the URI only (not the name!)

--- business/catalogue-service/src/main/java/eu/nimble/service/catalogue/impl/marmotta/MarmottaAdapter.java
+++ business/catalogue-service/src/main/java/eu/nimble/service/catalogue/impl/marmotta/MarmottaAdapter.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.marmotta.client.ClientConfiguration;
 import org.apache.marmotta.client.MarmottaClient;
@@ -16,6 +18,8 @@ import eu.nimble.service.catalogue.category.datamodel.Category;
 public class MarmottaAdapter {
 	private static final String MARMOTTA_URI = "http://134.168.33.237:8080/marmotta";
 	private static final String GRAPH_URI = "http://134.168.33.237:8080/marmotta/context/micuna";
+	
+	private static final String FURNITURE_NS = "http://www.semanticweb.org/ontologies/2013/4/Ontology1367568797694.owl#";
 
 	private MarmottaClient client;
 	public MarmottaAdapter() {
@@ -26,9 +30,9 @@ public class MarmottaAdapter {
 	}
 	
 	public static void main(String[]args) throws Exception {
-		List<Category> cats = new MarmottaAdapter().getCategoryByName("Tro");
+		List<Category> cats = new MarmottaAdapter().getCategoryByName("web");
 		for ( Category c : cats ) {
-			System.out.println(c.getPreferredName());
+			System.out.println(c.getCategoryUri() + " - " + c.getPreferredName());
 		}
 	}
 
@@ -39,24 +43,36 @@ public class MarmottaAdapter {
 				"PREFIX owl: <http://www.w3.org/2002/07/owl#>" + System.lineSeparator() +  
 				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" + System.lineSeparator() +  
 				"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " + System.lineSeparator() +  
-				"PREFIX mic: <http://www.semanticweb.org/ontologies/2013/4/Ontology1367568797694.>" + System.lineSeparator() +   
+				"PREFIX mic: <http://www.semanticweb.org/ontologies/2013/4/Ontology1367568797694.owl#>" + System.lineSeparator() +   
 				"" + System.lineSeparator() +   
 				"SELECT ?uri ?name WHERE {" + System.lineSeparator() +  
 				"  GRAPH <http://134.168.33.237:8080/marmotta/context/micuna> {" + System.lineSeparator() +  
 				"    ?uri rdf:type owl:Class . " + System.lineSeparator() +  
-				"    ?uri mic:owltranslation ?name ." + 
-				"    FILTER regex (?name, \"" + name + "\") ." + System.lineSeparator() +  
+				"    FILTER regex (?uri, \"" + name + "\") ." + System.lineSeparator() +  
 				"	}" + System.lineSeparator() +   
 				"}");
 		if (sparqlResult != null ) {
 			for ( int i = 0; i<  sparqlResult.size(); i++ ) {
 				Map<String, RDFNode> record = sparqlResult.get(i);
-				Category cat = new Category();
-				cat.setCategoryUri(record.get("uri").toString());
-				cat.setPreferredName(record.get("name").toString());
-				result.add(cat);
+				String remainder = getRemainder(record.get("uri").toString(), FURNITURE_NS);
+				if ( remainder.toLowerCase().contains(name.toLowerCase())) {
+					Category cat = new Category();
+					cat.setCategoryUri(record.get("uri").toString());
+					//
+					cat.setPreferredName(remainder);
+					result.add(cat);
+					
+				}
 			}
 		}
 		return result;
+		
+	}
+	
+	private String getRemainder(String value, String prefix) {
+		if ( value.startsWith(prefix)) {
+			return value.substring(prefix.length());
+		}
+		return value;
 	}
 }
