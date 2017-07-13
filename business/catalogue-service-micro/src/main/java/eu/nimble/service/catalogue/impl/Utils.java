677707509ab56026978b2af31a709911d0363730
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-07-13 13:39:10 +0200

    minor refactoring

--- business/catalogue-service-micro/src/main/java/eu/nimble/service/catalogue/impl/Utils.java
+++ business/catalogue-service-micro/src/main/java/eu/nimble/service/catalogue/impl/Utils.java
@@ -6,7 +6,7 @@ public class Utils {
 
     public static String baseUrl(HttpServletRequest request) {
         String baseUrl = request.getRequestURL().toString();
-        if(!baseUrl.contentEquals("/"))
+        if(!baseUrl.endsWith("/"))
             baseUrl += "/";
         return baseUrl;
     }
