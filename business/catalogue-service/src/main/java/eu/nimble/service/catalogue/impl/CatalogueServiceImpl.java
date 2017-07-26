cbd453ea79a1d156e5dfb4fed6a445b9b47c79ea
Suat Gonul <suat@srdc.com.tr>
2017-07-26 11:03:55 +0300

    added logs for RDF transformation and made storing the data in Marmotta configurable

--- business/catalogue-service/src/main/java/eu/nimble/service/catalogue/impl/CatalogueServiceImpl.java
+++ business/catalogue-service/src/main/java/eu/nimble/service/catalogue/impl/CatalogueServiceImpl.java
@@ -13,6 +13,7 @@ import eu.nimble.service.catalogue.category.datamodel.Property;
 import eu.nimble.service.catalogue.category.datamodel.Unit;
 import eu.nimble.service.catalogue.category.datamodel.Value;
 import eu.nimble.service.catalogue.exception.CatalogueServiceException;
+import eu.nimble.service.catalogue.util.ConfigUtil;
 import eu.nimble.service.model.modaml.catalogue.TEXCatalogType;
 import eu.nimble.service.model.ubl.catalogue.CatalogueType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.*;
@@ -36,13 +37,13 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.rmi.server.UID;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
 import static eu.nimble.service.catalogue.impl.TemplateConfig.*;
+import static eu.nimble.service.catalogue.util.ConfigUtil.*;
 
 /**
  * @author yildiray
@@ -231,13 +232,18 @@ public class CatalogueServiceImpl implements CatalogueService {
     }
 
     private void deleteCatalogueFromMarmotta(String uuid) {
+        boolean indexToMarmotta = Boolean.valueOf(ConfigUtil.getInstance().getConfig(CONFIG_CATALOGUE_PERSISTENCE_MARMOTTA_INDEX));
+        if(indexToMarmotta == false) {
+            logger.info("Index to Marmotta is set to false");
+            return;
+        }
+
         logger.info("Catalogue with uuid: {} will be deleted from Marmotta", uuid);
 
         URL marmottaURL;
         try {
-            Properties prop = new Properties();
-            prop.load(CatalogueServiceImpl.class.getClassLoader().getResourceAsStream("application.properties"));
-            marmottaURL = new URL(prop.getProperty("marmotta.url") + "/context/" + uuid);
+            String marmottaBaseUrl = ConfigUtil.getInstance().getConfig(CONFIG_CATALOGUE_PERSISTENCE_MARMOTTA_URL);
+            marmottaURL = new URL(marmottaBaseUrl + "/context/" + uuid);
         } catch (IOException e) {
             throw new CatalogueServiceException("Failed to read Marmotta URL from config file", e);
         }
@@ -425,7 +431,11 @@ public class CatalogueServiceImpl implements CatalogueService {
     }
 
     private XML2OWLMapper transformCatalogueToRDF(CatalogueType catalogue) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // TODO generate the ontology once, once the data model is finalized
+        XSD2OWLMapper mapping = getXSDToOWLMapping();
+
+        ByteArrayOutputStream serializedCatalogueBaos = new ByteArrayOutputStream();
+        StringWriter serializedCatalogueWriter = new StringWriter();
         try {
             String packageName = catalogue.getClass().getPackage().getName();
             JAXBContext jc = JAXBContext.newInstance(packageName);
@@ -434,61 +444,69 @@ public class CatalogueServiceImpl implements CatalogueService {
             marsh.setProperty("jaxb.formatted.output", true);
             JAXBElement element = new JAXBElement(
                     new QName(Configuration.UBL_CATALOGUE_NS, "Catalogue"), catalogue.getClass(), catalogue);
-            marsh.marshal(element, baos);
+            marsh.marshal(element, serializedCatalogueBaos);
+            marsh.marshal(element, serializedCatalogueWriter);
 
         } catch (JAXBException e) {
             throw new CatalogueServiceException("Failed to serialize the catalogue instance to XML", e);
         }
 
-            /*PrintWriter writer = new PrintWriter("serialized_ubl_catalogue.xml");
-            String serializedCatalogue = JAXBUtility.serialize(catalogue, "Catalogue");
-            writer.println(serializedCatalogue);
-            writer.flush();
-            writer.close();*/
+        // log the catalogue to be transformed
+        logger.debug("Catalogue to be transformed:\n {}", serializedCatalogueWriter.toString());
+        serializedCatalogueWriter.flush();
 
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(serializedCatalogueBaos.toByteArray());
+            serializedCatalogueBaos.flush();
+            XML2OWLMapper generator = new XML2OWLMapper(bais, mapping);
+            generator.convertXML2OWL();
+
+            serializedCatalogueBaos.close();
+            bais.close();
+
+            StringWriter catalogueRDFWriter = new StringWriter();
+            generator.writeModel(catalogueRDFWriter, "N3");
+            logger.debug("Transformed RDF data: {}", catalogueRDFWriter.toString());
+            catalogueRDFWriter.flush();
+
+            return generator;
+
+        } catch (IOException e) {
+            throw new CatalogueServiceException("Failed to convert catalogue with uuid " + catalogue.getUUID() + " to RDF", e);
+        }
+    }
+
+    private XSD2OWLMapper getXSDToOWLMapping() {
         URL url = CatalogueServiceImpl.class.getResource(Configuration.UBL_CATALOGUE_SCHEMA);
         XSD2OWLMapper mapping = new XSD2OWLMapper(url);
         mapping.setObjectPropPrefix("");
         mapping.setDataTypePropPrefix("");
         mapping.convertXSD2OWL();
 
-        FileOutputStream ont;
-        try {
-            File f = new File("ubl_catalogue_ontology.n3");
-            ont = new FileOutputStream(f);
-            mapping.writeOntology(ont, "N3");
-            ont.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // log the ontology generated based on the XSD schema
+        StringWriter serializedOntology = new StringWriter();
+        mapping.writeOntology(serializedOntology, "N3");
+        //logger.debug("Serialized ontology: {}", serializedOntology.toString());
+        //serializedOntology.flush();
 
-
-        XML2OWLMapper generator = new XML2OWLMapper(new ByteArrayInputStream(baos.toByteArray()), mapping);
-        generator.convertXML2OWL();
-
-        // This part prints the RDF data model to the specified file.
-            /*try {
-                File f = new File("ubl_catalogue_serv.n3");
-                FileOutputStream fout = new FileOutputStream(f);
-                generator.writeModel(fout, "RDF/XML");
-                fout.close();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }*/
-        return generator;
+        return mapping;
     }
 
     private void submitCatalogueDataToMarmotta(CatalogueType catalogue) {
+        boolean indexToMarmotta = Boolean.valueOf(ConfigUtil.getInstance().getConfig(CONFIG_CATALOGUE_PERSISTENCE_MARMOTTA_INDEX));
+        if(indexToMarmotta == false) {
+            logger.info("Index to Marmotta is set to false");
+            return;
+        }
+
         logger.info("Catalogue with uuid: {} will be submitted to Marmotta.", catalogue.getUUID());
         XML2OWLMapper rdfGenerator = transformCatalogueToRDF(catalogue);
         logger.info("Transformed catalogue with uuid: {} to RDF", catalogue.getUUID());
 
         URL marmottaURL;
         try {
-            Properties prop = new Properties();
-            prop.load(CatalogueServiceImpl.class.getClassLoader().getResourceAsStream("application.properties"));
-            marmottaURL = new URL(prop.getProperty("marmotta.url") + "/import/upload?context=" + catalogue.getUUID());
+            String marmottaBaseUrl = ConfigUtil.getInstance().getConfig(CONFIG_CATALOGUE_PERSISTENCE_MARMOTTA_URL);
+            marmottaURL = new URL(marmottaBaseUrl + "/import/upload?context=" + catalogue.getUUID());
         } catch (MalformedURLException e) {
             throw new CatalogueServiceException("Invalid format for the submitted template", e);
         } catch (IOException e) {
