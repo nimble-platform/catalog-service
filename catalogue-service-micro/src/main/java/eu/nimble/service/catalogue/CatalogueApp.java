695cc166b06ad6613b60e88d0a7899e065d3169c
Johannes Innerbichler <j.innerbichler@gmail.com>
2017-11-30 10:50:16 +0100

    added cors config for local develelopment

--- catalogue-service-micro/src/main/java/eu/nimble/service/catalogue/CatalogueApp.java
+++ catalogue-service-micro/src/main/java/eu/nimble/service/catalogue/CatalogueApp.java
@@ -1,6 +1,9 @@
 package eu.nimble.service.catalogue;
 
 import eu.nimble.service.catalogue.sync.MarmottaSynchronizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
@@ -9,12 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableCircuitBreaker
@@ -24,6 +31,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class CatalogueApp implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(CatalogueApp.class);
+
+    @Value("${nimble.corsEnabled:false}")
+    private String corsEnabled;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                if (corsEnabled.equals("true")) {
+                    logger.info("Enabling CORS...");
+                    registry.addMapping("/**").allowedOrigins("*");
+                }
+            }
+        };
+    }
 
     @Override
     public void run(String... arg0) throws Exception {
