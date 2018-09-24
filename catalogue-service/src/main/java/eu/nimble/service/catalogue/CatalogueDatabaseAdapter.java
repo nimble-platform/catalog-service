b52ef95beb29869163a6367e8421b40797776ee0
dogukan10 <dogukan10cav@gmail.com>
2018-09-24 15:31:29 +0300

    Moved getParty method from DataIntegratorUtil to CatalogueDatabaseAdapter

--- catalogue-service/src/main/java/eu/nimble/service/catalogue/CatalogueDatabaseAdapter.java
+++ catalogue-service/src/main/java/eu/nimble/service/catalogue/CatalogueDatabaseAdapter.java
@@ -1,5 +1,6 @@
 package eu.nimble.service.catalogue;
 
+import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
 import eu.nimble.utility.Configuration;
 import eu.nimble.utility.HibernateUtility;
 
@@ -11,5 +12,18 @@ public class CatalogueDatabaseAdapter {
         String queryStr = "SELECT COUNT(c) FROM CatalogueType c WHERE c.ID = ? and c.providerParty.ID = ?";
         int catalogueExists = ((Long) HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).load(queryStr, partySpecificCatalogueId, partyId)).intValue();
         return catalogueExists == 1 ? true : false;
+    }
+
+    public static PartyType getParty(PartyType party){
+        if(party == null){
+            return null;
+        }
+        String query = "SELECT party FROM PartyType party WHERE party.ID = ?";
+        PartyType partyType = HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).load(query,party.getID());
+        if(partyType == null){
+            HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).persist(party);
+            return party;
+        }
+        return partyType;
     }
 }
--- catalogue-service/src/main/java/eu/nimble/service/catalogue/util/DataIntegratorUtil.java
+++ catalogue-service/src/main/java/eu/nimble/service/catalogue/util/DataIntegratorUtil.java
@@ -1,13 +1,12 @@
 package eu.nimble.service.catalogue.util;
 
+import eu.nimble.service.catalogue.CatalogueDatabaseAdapter;
 import eu.nimble.service.model.ubl.catalogue.CatalogueType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.CatalogueLineType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.CommodityClassificationType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.DocumentReferenceType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
 import eu.nimble.service.model.ubl.commonbasiccomponents.CodeType;
-import eu.nimble.utility.Configuration;
-import eu.nimble.utility.HibernateUtility;
 
 public class DataIntegratorUtil {
 
@@ -42,24 +41,11 @@ public class DataIntegratorUtil {
     }
 
     public static void checkExistingParties(CatalogueType catalogue){
-        PartyType partyType = getParty(catalogue.getProviderParty());
+        PartyType partyType = CatalogueDatabaseAdapter.getParty(catalogue.getProviderParty());
         catalogue.setProviderParty(partyType);
         for (CatalogueLineType catalogueLineType : catalogue.getCatalogueLine()){
             catalogueLineType.getGoodsItem().getItem().setManufacturerParty(partyType);
         }
-    }
-
-    private static PartyType getParty(PartyType party){
-        if(party == null){
-            return null;
-        }
-        String query = "SELECT party FROM PartyType party WHERE party.ID = '"+party.getID()+"'";
-        PartyType partyType = HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).load(query);
-        if(partyType == null){
-            HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).persist(party);
-            return party;
-        }
-        return partyType;
     }
 
 }
