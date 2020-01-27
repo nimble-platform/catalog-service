72cf890bed10bf485cbea8984ef2723ab92453b2
dogukan10 <dogukan10cav@gmail.com>
2020-01-27 10:26:54 +0300

    created new role config for admin operations

--- catalogue-service/src/main/java/eu/nimble/service/catalogue/config/RoleConfig.java
+++ catalogue-service/src/main/java/eu/nimble/service/catalogue/config/RoleConfig.java
@@ -3,6 +3,8 @@ package eu.nimble.service.catalogue.config;
 import eu.nimble.utility.validation.NimbleRole;
 
 public class RoleConfig {
+    public static final NimbleRole[] REQUIRED_ROLES_FOR_ADMIN_OPERATIONS =
+            {NimbleRole.PLATFORM_MANAGER};
     public static final NimbleRole[] REQUIRED_ROLES_CATALOGUE =
             {NimbleRole.COMPANY_ADMIN,
                     NimbleRole.EXTERNAL_REPRESENTATIVE,
