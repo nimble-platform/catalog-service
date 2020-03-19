5a59cc1a0f9da9474f7aa2d4bb841b517fb2cffe
Suat Gonul <suat@srdc.com.tr>
2020-03-19 15:43:44 +0300

    added environment variable to enable selective integration of taxonomies

--- catalogue-service/src/main/java/eu/nimble/service/catalogue/category/TaxonomyManager.java
+++ catalogue-service/src/main/java/eu/nimble/service/catalogue/category/TaxonomyManager.java
@@ -7,62 +7,77 @@ import eu.nimble.service.catalogue.category.furnitureontology.FurnitureOntologyT
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class TaxonomyManager {
 
     private static Logger logger = LoggerFactory.getLogger(TaxonomyManager.class);
 
+    @Value("${nimble.catalog.category.enabled-taxonomies}")
+    private String enabledTaxonomies;
+
     private Map<String,TaxonomyQueryInterface> taxonomiesMap = new HashMap<>();
 
-    public TaxonomyManager() {
-        InputStream inputStream = null;
+    @PostConstruct
+    public void initTaxonomyManager() throws Exception {
+        // read the taxonomy configurations
         logger.info("Reading categories from Taxonomies.json file...");
-        inputStream =  TaxonomyManager.class.getResourceAsStream("/Taxonomies.json");
+        List<Taxonomy> taxonomies = new ArrayList<>();
+        InputStream inputStream = TaxonomyManager.class.getResourceAsStream("/Taxonomies.json");
         String fileContent = null;
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
             fileContent = IOUtils.toString(inputStream);
+            taxonomies = objectMapper.readValue(fileContent,new TypeReference<List<Taxonomy>>(){});
+            logger.info("Read categories from Taxonomies.json file");
         } catch (IOException e) {
             logger.error("Failed to read categories from Taxonomies.json file",e);
+            throw e;
         } finally {
             if(inputStream != null){
                 try {
                     inputStream.close();
                 } catch (IOException e) {
                     logger.error("Failed to close input stream",e);
+                    throw e;
                 }
             }
         }
 
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        // Taxonomies
-        try {
-            List<Taxonomy> taxonomies = objectMapper.readValue(fileContent,new TypeReference<List<Taxonomy>>(){});
-            // populate taxonomies map
-            for(Taxonomy taxonomy:taxonomies){
-                if(taxonomy.getId().contentEquals("eClass")){
-                    taxonomiesMap.put(taxonomy.getId(),new EClassTaxonomyQueryImpl(taxonomy));
-                }
-                else if(taxonomy.getId().contentEquals("FurnitureOntology")){
-                    taxonomiesMap.put(taxonomy.getId(),new FurnitureOntologyTaxonomyQueryImpl(taxonomy));
-                }
-                else{
-                    taxonomiesMap.put(taxonomy.getId(),new DefaultTaxonomyQueryImpl(taxonomy));
-                }
-                logger.info("Parsed {} taxonomy metadata", taxonomy.getId());
-            }
-            logger.info("Created taxonomies map");
-        } catch (IOException e) {
-            e.printStackTrace();
+        // get the enabled taxonomies
+        List<String> enabledTaxonomyIds = Arrays.asList(enabledTaxonomies.split(","));
+        for (int i=0; i<enabledTaxonomyIds.size(); i++) {
+            enabledTaxonomyIds.set(i, enabledTaxonomyIds.get(i).trim());
         }
+        logger.info("Enabled taxonomies are: {}", enabledTaxonomyIds);
+
+        // populate taxonomies map
+        for(Taxonomy taxonomy:taxonomies){
+            String taxonomyId = taxonomy.getId();
+            // do not consider non-enabled taxonomies
+            if (!enabledTaxonomyIds.contains(taxonomyId)) {
+                continue;
+            }
+
+            if(taxonomy.getId().contentEquals("eClass")){
+                taxonomiesMap.put(taxonomy.getId(),new EClassTaxonomyQueryImpl(taxonomy));
+            }
+            else if(taxonomy.getId().contentEquals("FurnitureOntology")){
+                taxonomiesMap.put(taxonomy.getId(),new FurnitureOntologyTaxonomyQueryImpl(taxonomy));
+            }
+            else{
+                taxonomiesMap.put(taxonomy.getId(),new DefaultTaxonomyQueryImpl(taxonomy));
+            }
+            logger.info("Parsed {} taxonomy metadata", taxonomy.getId());
+        }
+        logger.info("Created taxonomies map");
     }
 
     public Map<String, TaxonomyQueryInterface> getTaxonomiesMap() {
